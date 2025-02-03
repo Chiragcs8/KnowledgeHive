318a5def22536622de79b634c86b129b45d52631
Chiragcs8 <chirag98589@gmail.com>
2025-02-03 22:15:31 +0530

    simple change

--- Frontend/src/config/index.jsx
+++ Frontend/src/config/index.jsx
@@ -1,52 +1,54 @@
 export const signUpFormControls = [
   {
-    name: "userName",
-    label: "User Name",
-    placeholder: "Enter your username",
-    type: "text",
-    componentType: "input",
+      name : 'userName',
+      label : 'User Name',
+      placeholder : 'Enter your username',
+      type : 'text',
+      componentType : 'input',
   },
   {
-    name: "userEmail",
-    label: "User Email",
-    placeholder: "Enter your Email",
-    type: "email",
-    componentType: "input",
+      name : 'userEmail',
+      label : 'User Email',
+      placeholder : 'Enter your Email',
+      type : 'email',
+      componentType : 'input',
   },
   {
-    name: "password",
-    label: "Password",
-    placeholder: "Enter your password",
-    type: "password",
-    componentType: "input",
+      name : 'password',
+      label : 'Password',
+      placeholder : 'Enter your password',
+      type : 'password',
+      componentType : 'input',
   },
 ]; // array of form control and objects for signup form
 
+
 export const signInFormControls = [
   {
-    name: "userEmail",
-    label: "User Email",
-    placeholder: "Enter your Email",
-    type: "email",
-    componentType: "input",
+      name : 'userEmail',
+      label : 'User Email',
+      placeholder : 'Enter your Email',
+      type : 'email',
+      componentType : 'input',
   },
   {
-    name: "password",
-    label: "Password",
-    placeholder: "Enter your password",
-    type: "password",
-    componentType: "input",
+      name : 'password',
+      label : 'Password',
+      placeholder : 'Enter your password',
+      type : 'password',
+      componentType : 'input',
   },
 ]; // array of form control and objects for signin form
 
 export const initialSignInFormData = {
-  userEmail: "",
-  password: "",
+  userEmail : "",
+  password : "",
 };
+
 export const initialSignUpFormData = {
-  userName: "",
-  userEmail: "",
-  password: "",
+  userName : "",
+  userEmail : "",
+  password : "",
 };
 
 export const languageOptions = [
@@ -169,4 +171,4 @@ export const courseCurriculumInitialFormData = [
     freePreview : false,
     public_id : "",
   }
-]
+];
