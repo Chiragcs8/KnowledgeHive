be3a0c4e10ee659de7ab1c50b74637f4ad33822f
Chiragcs8 <chirag98589@gmail.com>
2025-03-02 00:32:35 +0530

    completed order controller

--- Backend/controllers/student-controller/order-controller.js
+++ Backend/controllers/student-controller/order-controller.js
@@ -1,9 +1,7 @@
-
-const paypal = require('../../helpers/paypal.js')
-const Order = require('../../models/Order.js')
-const StudentCourses = require('../../models/StudentCourses.js')
-const Course = require('../../models/Course.js')
-
+const paypal = require("../../helpers/paypal.js");
+const Order = require("../../models/Order.js");
+const StudentCourses = require("../../models/StudentCourses.js");
+const Course = require("../../models/Course.js");
 
 const createOrder = async (req, res) => {
   try {
@@ -25,47 +23,80 @@ const createOrder = async (req, res) => {
       coursePricing,
     } = red.body;
 
-    const create_payment_json={
-        intent:'sale',
-        payer:{
-            payment_method: 'paypal'
-        }, 
-        redirect_url: {
-            return_url: `${process.env.CLIENT_URL}/payment-return`,
-            cancel_url: `${process.env.CLIENT_URL}/payment-cancel`
+    const create_payment_json = {
+      intent: "sale",
+      payer: {
+        payment_method: "paypal",
+      },
+      redirect_url: {
+        return_url: `${process.env.CLIENT_URL}/payment-return`,
+        cancel_url: `${process.env.CLIENT_URL}/payment-cancel`,
+      },
+      transactions: [
+        {
+          item_list: {
+            items: [
+              {
+                name: courseTitle,
+                sku: courseId,
+                price: coursePricing,
+                currency: "USD",
+                quantity: 1,
+              },
+            ],
+          },
+          amount: {
+            currency: "USD",
+            total: coursePricing.toFixed(2),
+          },
+          description: courseTitle,
         },
-        transactions : [
-            {
-                item_list : {
-                    items : [
-                        {
-                            name : courseTitle,
-                            sku : courseId,
-                            price : coursePricing,
-                            currency : 'USD',
-                            quantity : 1
-                        }
-                    ]
-                },
-                amount : {
-                    currency : 'USD',
-                    total : coursePricing.toFixed(2),
-                },
-                description : courseTitle
-            },
-        ]
-    }
+      ],
+    };
 
-    paypal.payment.create(create_payment_json, async(error, paymentInfo)=>{
-      if(error){
+    paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
+      if (error) {
         console.log(error);
-        
+
         return res.status(500).json({
           success: false,
           message: "Error while creating payment!",
-        })
+        });
+      } else {
+        const newlyCreatedCourseOrder = new Order({
+          userId,
+          userName,
+          userEmail,
+          orderstatus,
+          paymentMethod,
+          paymentStatus,
+          orderDate,
+          paymentId,
+          payerId,
+          instructorId,
+          instructorName,
+          courseImage,
+          courseTitle,
+          courseId,
+          coursePricing,
+        });
+
+        await newlyCreatedCourseOrder.save();
+
+        const approveUrl = paymentInfo.links.find(
+          (link) => link.rel == "approvel_url"
+        ).href;
+
+        res.status(201).json({
+          success: true,
+          data: {
+            approveUrl,
+            orderId: newlyCreatedCourseOrder._id,
+          },
+          message: "Payment created successfully!",
+        });
       }
-    })
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -77,6 +108,70 @@ const createOrder = async (req, res) => {
 
 const capturePaymentAndFinalizeOrder = async (req, res) => {
   try {
+    const { paymentId, payerId, orderId } = req.body;
+
+    let order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order can not be found!",
+      });
+    }
+
+    order.paymentStatus = "paid";
+    order.orderstatus = "confirmed";
+    order.paymentId = paymentId;
+    order.payerId = payerId;
+
+    await order.save();
+
+    //update out student course model
+    const studentCourses = await studentCourses.findOne({
+      userId: order.userId,
+    });
+
+    if (studentCourses) {
+      studentCourses.courses.push({
+        courseId: order.courseId,
+        title: order.courseTitle,
+        instructorId: order.instructorId,
+        instructorName: order.instructorName,
+        dateOfPurchase: order.orderDate,
+        courseImage: order.courseImage,
+      });
+      await studentCourses.save();
+    } else {
+      const newStudentCourses = new StudentCourses({
+        userId: order.userId,
+        courses: [
+          {
+            courseId: order.courseId,
+            title: order.courseTitle,
+            instructorId: order.instructorId,
+            instructorName: order.instructorName,
+            dateOfPurchase: order.orderDate,
+            courseImage: order.courseImage,
+          },
+        ],
+      });
+      await newStudentCourses.save();
+    }
+
+    // update course schema students
+    await Course.findByIdAndUpdate(order.courseId, {
+      $addToSet: {
+        studentId: order.userId,
+        studentName: order.userName,
+        studentEmail: order.userEmail,
+        paidAmount: order.coursePricing,
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Payment captured and order finalized successfully!",
+      data: order,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({
