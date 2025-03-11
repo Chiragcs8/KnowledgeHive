70ccb9519b3e3068c8ed1bdc6f636c4d55bd28e5
Chiragcs8 <chirag98589@gmail.com>
2025-03-11 23:15:06 +0530

    improvising small feature

--- Frontend/src/components/student-view/header.jsx
+++ Frontend/src/components/student-view/header.jsx
@@ -24,7 +24,11 @@ function StudentViewCommonHeader() {
           <Button
             variant="ghost"
             className="text-[14px] md:text-[16mx] font-medium"
-            onClick={()=> navigate("/courses")}
+            onClick={()=> 
+              location.pathname.includes('/courses') 
+              ? null
+              : navigate('/courses')
+            }
           >
             Explore Courses
           </Button>
