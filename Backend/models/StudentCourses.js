7e70c862383ad67f35e6105cdc517881df93ebf3
Chiragcs8 <chirag98589@gmail.com>
2025-03-13 23:16:24 +0530

    fixed a problem

--- Backend/models/StudentCourses.js
+++ Backend/models/StudentCourses.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const StudentCorsesSchema = new mongoose.Schema({
-    userId: String,
-    courses : [
-        {
-            courseId: String,
-            title : String,
-            instructorId : String,
-            instructorName : String,
-            dateOfPurchase : Date,
-            courseImage : String
-        }
-    ]
-})
+const StudentCoursesSchema = new mongoose.Schema({
+  userId: String,
+  courses: [
+    {
+      courseId: String,
+      title: String,
+      instructorId: String,
+      instructorName: String,
+      dateOfPurchase: Date,
+      courseImage: String,
+    },
+  ],
+});
 
-module.exports = mongoose.model('StudentCourses', StudentCorsesSchema);
+module.exports = mongoose.model("StudentCourses", StudentCoursesSchema);
